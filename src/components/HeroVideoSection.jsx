641f8e18fd7cd55fc7d45c25e56b7a11b77c1717
양윤직 <yang-yunjig@yang-yunjig-ui-MacBookPro.local>
2025-06-18 12:48:38 +0900

    6. 18. ver.2

--- src/components/HeroVideoSection.jsx
+++ src/components/HeroVideoSection.jsx
@@ -29,8 +29,8 @@ export default function HeroVideoSection({ setVideoIndex }) {
     <section
       id="hero-video"
       // className="fixed inset-0 w-screen h-screen overflow-hidden z-10" style={{height: '100dvh'}}
-      className="fixed top-0 left-0 w-[100vw] h-[100dvh] overflow-hidden z-10"
-    >
+      className="fixed top-0 left-0 w-[100vw] h-[100dvh] overflow-hidden z-10">
+        
       <video
         ref={videoRef}
         className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 ${fade ? 'opacity-100' : 'opacity-0'
@@ -50,7 +50,7 @@ export default function HeroVideoSection({ setVideoIndex }) {
         <div className="w-6 h-10 border-2 border-white rounded-full flex justify-center items-start p-1">
           <div className="w-1 h-2 bg-white rounded-full animate-ping" />
         </div>
-         {/* 텍스트 */}
+        {/* 텍스트 */}
         <span className="mt-2 text-sm tracking-widest">click</span>
         {/* 아래 화살표 */}
         <span className="text-xl animate-bounce">↓</span>
