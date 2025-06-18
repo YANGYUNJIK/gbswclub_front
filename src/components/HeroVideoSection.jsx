2cf5cc89f1bb889b3c41a825e225f6e98b61edb3
양윤직 <yang-yunjig@yang-yunjig-ui-MacBookPro.local>
2025-06-18 11:15:27 +0900

    6. 18. ver.2

--- src/components/HeroVideoSection.jsx
+++ src/components/HeroVideoSection.jsx
@@ -45,7 +45,7 @@ export default function HeroVideoSection({ setVideoIndex }) {
       </video>
 
       {/* 🔽 스크롤 유도 마우스 + 텍스트 */}
-      <div className="absolute bottom-5 right-6 z-20 flex flex-col items-center text-white animate-bounce opacity-80">
+      <div className="absolute bottom-5 right-5 z-20 flex flex-col items-center text-white animate-bounce opacity-80">
         <div className="w-6 h-10 border-2 border-white rounded-full flex justify-center items-start p-1">
           <div className="w-1 h-2 bg-white rounded-full animate-ping" />
         </div>
